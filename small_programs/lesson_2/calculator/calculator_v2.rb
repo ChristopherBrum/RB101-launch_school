bf1ec103f6b560d2e678868bc642b20ebf56ecfe
Christopher Brum <christopher.brum@gmail.com>
2021-02-05 09:31:26 -0800

    cleaned up calculator_v2.rb ready to submit

--- small_programs/lesson_2/calculator/calculator_v2.rb
+++ small_programs/lesson_2/calculator/calculator_v2.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'pry'
 
 MESSAGES = YAML.load_file('calculator_messages.yml')
 
@@ -11,18 +12,18 @@ def prompt(key, additional_input='')
     puts key
   elsif additional_input.length != 0
     puts "=> #{key} #{additional_input}"
-  else 
+  else
     puts "=> #{key}"
-  end 
+  end
 end
 
-def greeting(lang)
+def reset_console
   system 'clear'
-  prompt(messages('greeting', lang))
 end
 
-# Language selection and validation
+# ~~~~~~~~~~~~~~~~~~ Language selection and validation ~~~~~~~~~~~~~~~~~~
 def select_language
+  system 'clear'
   prompt(messages('choose_lang'))
   lang_choice = gets.chomp
   if valid_lang_choice?(lang_choice)
@@ -44,11 +45,10 @@ def set_lang(lang_selection)
   when '2'
     language = 'es'
   end
-
   language
 end
 
-# Name input and validation
+# ~~~~~~~~~~~~~~~~~~ Name input and validation ~~~~~~~~~~~~~~~~~~
 def get_name(lang)
   prompt(messages('get_name', lang))
   name = gets.chomp
@@ -63,13 +63,13 @@ def valid_name?(name, lang)
   end
 end
 
-# Number and FLoat input and validation
+# ~~~~~~~~~~~~~~~~~~ Number and Float input and validation ~~~~~~~~~~~~~~~~~~
 def valid_number?(num)
   true unless /\D/.match(num)
 end
 
 def valid_float?(num)
-  num.to_f.to_s == num 
+  num.to_f.to_s == num
 end
 
 def number?(num)
@@ -80,93 +80,121 @@ def valid_operator?(op)
   true if %w(1 2 3 4).include?(op)
 end
 
-def operation_to_message(op)
-  output =  case op
-              when '1'
-                output = messages('adding')
-              when '2'
-                output = messages('subtracting')
-              when '3'
-                output = messages('multiplying')
-              when '4'
-                output = messages('dividing')
-            end
+def get_num(message, lang)
+  num = ''
+  loop do
+    prompt(messages(message, lang))
+    num = gets.chomp
 
-  output
+    if number?(num)
+      break
+    else
+      prompt(messages('num_error', lang))
+    end
+  end
+  num
 end
 
-
-# Program begines running here:
-
-lang = select_language()
-greeting(lang)
-get_name(lang)
-
-
-num1 = nil
-loop do
-  # num1 input and validation
-  loop do
-    prompt(messages('get_num1', lang))
-    num1 = gets.chomp
-
-    if number?(num1)
-      break
-    else
-      prompt(messages('num_error'))
-    end
-  end
-
-  # num2 input and validation
-  num2 = nil
-  loop do
-    prompt(messages('get_num2'))
-    num2 = gets.chomp
-
-    if number?(num2)
-      break
-    else
-      prompt(messages('num_error'))
-    end
-  end
-
-  # operator input and validation
+def get_operator(lang)
   operator = ''
   loop do
-    prompt(messages('operator_prompt'))
+    prompt(messages('operator_prompt', lang))
     operator = gets.chomp
 
     if valid_operator?(operator)
       break
     else
-      prompt(messages('operator_error'))
+      prompt(messages('operator_error', lang))
     end
   end
+  operator
+end
 
-  result =  case operator
+def calculate(operator, num1, num2)
+  result = nil
+  case operator
+  when '1'
+    result = num1.to_i + num2.to_i
+  when '2'
+    result = num1.to_i - num2.to_i
+  when '3'
+    result = num1.to_i * num2.to_i
+  when '4'
+    result = num1.to_f / num2.to_f
+  end
+  result
+end
+
+# ~~~~~~~~~~~~~~~~~~ Results & Display Messaging ~~~~~~~~~~~~~~~~~~
+def greeting(lang)
+  reset_console
+  prompt(messages('greeting', lang))
+end
+
+def operation_to_message(op, lang)
+  output =  case op
             when '1'
-              num1.to_i + num2.to_i
+              messages('adding', lang)
             when '2'
-              num1.to_i - num2.to_i
+              messages('subtracting', lang)
             when '3'
-              num1.to_i * num2.to_i
+              messages('multiplying', lang)
             when '4'
-              num1.to_f / num2.to_f
+              messages('dividing', lang)
             end
+  output
+end
 
-  prompt(operation_to_message(operator) + messages('operation_calc_indicator'))
-  10.times do 
+def processing_data(operator, lang)
+  prompt(operation_to_message(operator, lang) + messages('calc_phrase', lang))
+  calc_arrow
+end
+
+def calc_arrow
+  10.times do
     sleep 0.1
     print '='
     sleep 0.1
-    print '-' 
+    print '-'
   end
-  puts
-  prompt(messages('return_result'), result.to_s)
-
-  prompt(messages('another_calc'))
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  puts '>'
 end
 
-prompt(messages('farewell'))
+def show_result(result, lang)
+  prompt(messages('return_result', lang), result.to_s)
+end
+
+def calc_again(lang)
+  prompt(messages('another_calc', lang))
+  answer = gets.chomp
+  answer
+end
+
+def valid_calc_again?(answer)
+  true if %w(Y y yes Yes YES si SI Si s).include?(answer)
+end
+
+def farewell(lang)
+  prompt(messages('farewell', lang))
+end
+
+# ~~~~~~~~~~~~~~~~~~ Program begins ~~~~~~~~~~~~~~~~~~
+
+lang = select_language()
+greeting(lang)
+get_name(lang)
+
+loop do
+  num1 = get_num('get_num1', lang)
+  num2 = get_num('get_num2', lang)
+  operator = get_operator(lang)
+  result = calculate(operator, num1, num2)
+
+  processing_data(operator, lang)
+  show_result(result, lang)
+
+  answer = calc_again(lang)
+  break unless valid_calc_again?(answer)
+end
+
+farewell(lang)
