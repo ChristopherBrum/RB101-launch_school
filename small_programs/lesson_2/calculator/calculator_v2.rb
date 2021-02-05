e779737dcd1ebfbc3e698d8f97bd5405a6d99931
Christopher Brum <christopher.brum@gmail.com>
2021-02-04 17:04:59 -0800

    pedac work with study group, work on calculator bonus features

--- small_programs/lesson_2/calculator/calculator_v2.rb
+++ small_programs/lesson_2/calculator/calculator_v2.rb
@@ -1,14 +1,71 @@
-# Inputs: 2 integers
-#         string or integer input choosing math operator
-# Outputs: Display integer results
-
 require 'yaml'
-MESSAGES = YAML.load_file("messages.yml")
 
-def prompt(message)
-  puts "=> #{message}"
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+LANGUAGE = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
 end
 
+def prompt(key, additional_input='')
+  if key == MESSAGES['en']['greeting']
+    puts key
+  elsif additional_input.length != 0
+    puts "=> #{key} #{additional_input}"
+  else 
+    puts "=> #{key}"
+  end 
+end
+
+def greeting(lang)
+  system 'clear'
+  prompt(messages('greeting', lang))
+end
+
+# Language selection and validation
+def select_language
+  prompt(messages('choose_lang'))
+  lang_choice = gets.chomp
+  if valid_lang_choice?(lang_choice)
+    set_lang(lang_choice)
+  else
+    prompt(messages('lang_error'))
+    select_language
+  end
+end
+
+def valid_lang_choice?(lang_choice)
+  %w(1 2).include?(lang_choice)
+end
+
+def set_lang(lang_selection)
+  case lang_selection
+  when '1'
+    language = 'en'
+  when '2'
+    language = 'es'
+  end
+
+  language
+end
+
+# Name input and validation
+def get_name(lang)
+  prompt(messages('get_name', lang))
+  name = gets.chomp
+  valid_name?(name, lang)
+end
+
+def valid_name?(name, lang)
+  if name.empty?
+    prompt(messages('name_error', lang))
+  else
+    prompt(messages('hi', lang), name)
+  end
+end
+
+# Number and FLoat input and validation
 def valid_number?(num)
   true unless /\D/.match(num)
 end
@@ -22,85 +79,69 @@ def number?(num)
 end
 
 def valid_operator?(op)
-  true if ['1', '2', '3', '4'].include?(op)
+  true if %w(1 2 3 4).include?(op)
 end
 
 def operation_to_message(op)
   output =  case op
               when '1'
-                output = 'Adding'
+                output = messages('adding')
               when '2'
-                output = 'Subtracting'
+                output = messages('subtracting')
               when '3'
-                output = 'Multiplying'
+                output = messages('multiplying')
               when '4'
-                output = 'Dividing'
+                output = messages('dividing')
             end
 
   output
 end
 
-def greeting()
-  prompt(MESSAGES["greeting"])
-  name = gets.chomp
-  valid_name?(name)
-end
 
-def valid_name?(name)
-  if name.empty?
-    prompt("Make sure to enter a valid name.")
-  else
-    prompt("Hi #{name}.")
-  end
-end
+# Program begines running here:
 
-greeting
+lang = select_language()
+greeting(lang)
+get_name(lang)
+
 
 num1 = nil
 loop do
   # num1 input and validation
   loop do
-    prompt(MESSAGES['get_num1'])
+    prompt(messages('get_num1', lang))
     num1 = gets.chomp
 
     if number?(num1)
       break
     else
-      prompt(MESSAGES["number_error"])
+      prompt(messages('num_error'))
     end
   end
 
   # num2 input and validation
   num2 = nil
   loop do
-    prompt("What's your second number?")
+    prompt(messages('get_num2'))
     num2 = gets.chomp
 
     if number?(num2)
       break
     else
-      prompt(MESSAGES["number_error"])
+      prompt(messages('num_error'))
     end
   end
 
   # operator input and validation
   operator = ''
   loop do
-    operator_prompt = <<-MSG
-    What operation would you like to perform? 
-    1)Add  
-    2)Subtract  
-    3)Multiply  
-    4)Divide
-    MSG
-
-    prompt(operator_prompt)
+    prompt(messages('operator_prompt'))
     operator = gets.chomp
 
     if valid_operator?(operator)
       break
     else
-      prompt("Hmm... that doesn't look like a valid input.")
+      prompt(messages('operator_error'))
     end
   end
 
@@ -115,13 +156,13 @@ loop do
               num1.to_f / num2.to_f
             end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+  prompt(operation_to_message(operator) + messages('operation_calc_indicator'))
   sleep 1.5
-  prompt("The result is #{result}")
+  prompt(messages('return_result'), result.to_s)
 
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  prompt(messages('another_calc'))
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thanks for using Calculator. Byeeeeeeeeee!")
+prompt(messages('farewell'))
