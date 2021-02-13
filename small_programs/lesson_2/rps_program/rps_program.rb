2338b13de18e845890bf9fca154a7fbe808e67ff
Christopher Brum <christopher.brum@gmail.com>
2021-02-12 17:42:21 -0800

    rock paper scissor file basic funtionality

---
+++ small_programs/lesson_2/rps_program/rps_program.rb
@@ -0,0 +1,50 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def test_method()
+  prompt("Hello there")
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_result(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+    (player == 'paper' && computer == 'rock') ||
+    (player == 'scissors' && computer == 'paper')
+    prompt("You win!")
+  elsif (player == 'scissors' && computer == 'rock') ||
+        (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors')
+    prompt("Computer Won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  system('clear')
+  choice = ''
+  computer_choice = VALID_CHOICES.sample
+
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+    
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+
+  display_result(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thanks for playing. Goodbye!")
