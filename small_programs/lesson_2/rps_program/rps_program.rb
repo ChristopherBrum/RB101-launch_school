e020f5618a0a8ecf49a9b55d0690f0654f812468
Christopher Brum <christopher.brum@gmail.com>
2021-02-16 08:34:20 -0800

    rps scissors program refactorting

--- small_programs/lesson_2/rps_program/rps_program.rb
+++ small_programs/lesson_2/rps_program/rps_program.rb
@@ -1,6 +1,6 @@
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
-def test_method()
+def test_method
   prompt("Hello there")
 end
 
@@ -10,8 +10,8 @@ end
 
 def display_result(player, computer)
   if (player == 'rock' && computer == 'scissors') ||
-    (player == 'paper' && computer == 'rock') ||
-    (player == 'scissors' && computer == 'paper')
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
     prompt("You win!")
   elsif (player == 'scissors' && computer == 'rock') ||
         (player == 'rock' && computer == 'paper') ||
@@ -30,12 +30,7 @@ loop do
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
-    
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+    VALID_CHOICES.include?(choice) ? break : prompt("Not a valid choice.")
   end
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
