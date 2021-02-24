b79a0b952a86899622d2640ec10c884e41ebe1e3
Christopher Brum <christopher.brum@gmail.com>
2021-02-24 08:04:24 -0800

    pedac practice

---
+++ lesson_4/pedac_solution.rb
@@ -0,0 +1,18 @@
+def sum_even_number_row(row_number)
+
+end
+
+def create_row(start_integer, row_length)
+  row = []
+  
+  row
+end
+
+## Test Cases ##
+p sum_even_number_row(1) == 2 # true
+p sum_even_number_row(2) == 10 # true
+p sum_even_number_row(4) == 68 # true
+
+p create_row(2, 1) == [2] # true
+p create_row(4, 2) == [4, 6] # true
+p create_row(8, 3) == [8, 10, 12] # true
