2a3e903b5020493361f71f61551907cce148747b
Christopher Brum <christopher.brum@gmail.com>
2021-02-25 18:35:29 -0800

    lesson 4 practice problems

---
+++ lesson_4/practice_problems.rb
@@ -0,0 +1,45 @@
+# What is the return value of the select method below? Why?
+
+# [1, 2, 3].select do |num|
+#   num > 5
+#   'hi'
+# end
+
+# This will return 
+
+
+
+################
+
+# flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+
+# hsh = flintstones.map do |name|
+#   arr = []
+#   arr.push(name)
+#   arr.push(flintstones.index(name))
+#   arr
+# end
+
+# p hsh.to_h
+
+
+
+################
+# Add up all of the ages from the Munster family hash:
+
+ages = { 
+  "Herman" => 32, 
+  "Lily" => 30, 
+  "Grandpa" => 5843, 
+  "Eddie" => 10, 
+  "Marilyn" => 22, 
+  "Spot" => 237 
+}
+
+combined_ages = 0
+
+ages.each do |name, age|
+  
+end
+
