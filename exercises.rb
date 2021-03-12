f7595eae6526bf81d6b1718a37a7d08f392cea16
Christopher Brum <christopher.brum@gmail.com>
2021-03-11 16:09:02 -0800

    More exercies completed

--- exercises.rb
+++ exercises.rb
@@ -221,62 +221,542 @@ Algorithm:
 
 =end
 
-def dms(num)
-  degree = num.to_i
-  p num, degree
-  # minutes = add_zero(minutes.to_i)
-  # seconds = add_zero(seconds)
-  #puts "#{degree}˚#{minutes}'#{seconds}\""
+
+# NEED TO RE-WORK THIS PROBLEM
+
+
+###############
+
+# Write a method that takes an array of strings, and returns an array of the 
+# same string values, except with the vowels (a, e, i, o, u) removed.
+
+=begin
+Understannding the Problem:
+  -Input: Array of strings
+  -Output: Array of strings with the vowels removed
+
+  -Rules:
+    -Method will take an array strings as an argument
+    -The method will remove the vowels from the strings within the array
+    -Return the array of strings without vowels 
+    -Case insensetive
+    -If a string contains all vowels it will return an empty string
+
+Test Cases:
+  remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+  remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+  remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+Data Structures:
+  -Arrays
+
+Algorithm:
+  -Iterate through the array of strings
+    -substitute any vowel characters for an empty string
+  -return updated array
+
+=end
+
+def remove_vowels(arr)
+  arr.map { |str| str.gsub(/[aeiou]/i, '') }
 end
 
-# def add_zero(integer)
-#   p integer.to_s
-#   if integer.to_s.length == 1
-#     integer = '0' + integer.to_s 
-#   elsif integer.to_s.length < 1
-#     integer = '00'
-#   end
-# end
-
-# dms(30) == %(30°00'00")
-# dms(76.73) == %(76°43'48")
-# dms(254.6) == %(254°36'00")
-dms(93.034773) == %(93°02'05")
-#dms(0) == %(0°00'00")
-#dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
-
-###############
-
-=begin
-Understannding the Problem:
-  -Input:
-  -Output:
-
-  -Rules:
-
-Test Cases:
-
-Data Structures:
-
-Algorithm:
-
-=end
-
+# remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+# remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+# remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
 
 
 ###############
 
 =begin
+
+The Fibonacci series is a series of numbers (1, 1, 2, 3, 5, 8, 13, 21, ...) such 
+that the first 2 numbers are 1 by definition, and each subsequent number is the 
+sum of the two previous numbers. This series appears throughout the natural world.
+
+Computationally, the Fibonacci series is a very simple series, but the results grow 
+at an incredibly rapid rate. For example, the 100th Fibonacci number is 
+354,224,848,179,261,915,075 -- that's enormous, especially considering that it takes 
+6 iterations before it generates the first 2 digit number.
+
+Write a method that calculates and returns the index of the first Fibonacci number 
+that has the number of digits specified as an argument. (The first Fibonacci number has index 1.)
+
 Understannding the Problem:
-  -Input:
-  -Output:
+  -Input: integer
+  -Output: integer
 
   -Rules:
+    -Method will take in an integer as an argument
+    -The input integer is the desired length of the digits output by the fibonacci sequence
+      -ex: input_integer = 2    =>   1 1 2 3 5 8 13    
+      -the first integer with the length of 2 digits is 13 which is the 7th digit in the fibonacci sequence
+    -return the index of the first instance of a number that matches the length of the input_integer
 
 Test Cases:
+  find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+  find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+  find_fibonacci_index_by_length(10) == 45
+  find_fibonacci_index_by_length(100) == 476
+  find_fibonacci_index_by_length(1000) == 4782
+  find_fibonacci_index_by_length(10000) == 47847
 
 Data Structures:
+  -Array, integers, strings
 
 Algorithm:
+  -array = []
+  -set 'base' to 1
+  -set 'current_sum' to 0
+  -set 'updated_sum' to 0
+  -loop while 'sum' to_s is less than 'input_int' 
+    -if base == 0 += current_sum
+    -current_sum
+    -updated_sum
+
 
 =end
+
+def find_fibonacci_index_by_length(output_length)
+  fibonacci_arr = []
+  first_num = 0
+  second_num = 1
+
+  loop do
+    first_num, second_num = second_num, first_num + second_num
+    fibonacci_arr << first_num
+    if output_length  == second_num.to_s.length 
+      fibonacci_arr << second_num
+      break
+    end
+  end
+  fibonacci_arr.length
+end
+
+# find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+# find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+# find_fibonacci_index_by_length(10) == 45
+# find_fibonacci_index_by_length(100) == 476
+# find_fibonacci_index_by_length(1000) == 4782
+# find_fibonacci_index_by_length(10000) == 47847
+
+###############
+
+=begin
+
+Write a method that takes an Array as an argument, and reverses its elements in place; 
+that is, mutate the Array passed into this method. The return value should be the 
+same Array object.
+
+You may not use Array#reverse or Array#reverse!.
+
+Note: for the test case list = ['abc'], we want to reverse the elements in the array. 
+The array only has one element, a String, but we're not reversing the String itself, 
+so the reverse! method call should return ['abc'].
+
+Understannding the Problem:
+  -Input: Array
+  -Output: Same array object with elements reversed
+
+  -Rules:
+    -Takes in an array of elements
+    -Reverse elements in place, meaning keeping the same array object reverse the elements within it. 
+    -Return value should be the same array object at the input
+    -Do not use #reverse or #reverse!
+    -We're reversing the elements within the array, not the elements themselves
+
+Test Cases:
+  list = [1,2,3,4]
+  result = reverse!(list)
+  result == [4, 3, 2, 1] # true
+  list == [4, 3, 2, 1] # true
+  list.object_id == result.object_id # true
+
+  list = %w(a b e d c)
+  reverse!(list) == ["c", "d", "e", "b", "a"] # true
+  list == ["c", "d", "e", "b", "a"] # true
+
+  list = ['abc']
+  reverse!(list) == ["abc"] # true
+  list == ["abc"] # true
+
+  list = []
+  reverse!(list) == [] # true
+  list == [] # true
+
+Data Structures:
+  -arrays
+
+Algorithm:
+  -iterate through array starting from second to last element working forwards
+    -delete second to last element
+      -using the deleted elements return value, push to end of array
+    -continue to the first element
+  -return original array
+
+=end
+
+def reverse!(arr)
+  (2..(arr.length)).each do |index|
+    arr.push(arr.delete(arr[-index]))
+  end
+  arr
+end
+
+# list = [1,2,3,4]
+# result = reverse!(list)
+# p result == [4, 3, 2, 1] # true
+# p list == [4, 3, 2, 1] # true
+# p list.object_id == result.object_id # true
+
+# list = %w(a b e d c)
+# p reverse!(list) == ["c", "d", "e", "b", "a"] # true
+# p list == ["c", "d", "e", "b", "a"] # true
+
+# list = ['abc']
+# p reverse!(list) == ["abc"] # true
+# p list == ["abc"] # true
+
+# list = []
+# p reverse!(list) == [] # true
+# p list == [] # true
+
+###############
+
+=begin
+
+Write a method that takes an Array, and returns a new Array with the elements of 
+the original list in reverse order. Do not modify the original list.
+
+You may not use Array#reverse or Array#reverse!, nor may you use the method you 
+wrote in the previous exercise.
+
+Understannding the Problem:
+  -Input: Array
+  -Output: New array with elements of orignal array reversed
+
+  -Rules:
+    -Method takes in an array as an argument
+    -creates new array
+    -populates new array with elements of original array reversed
+    -does not mutate the original array
+    -Do not use #reverese or #reverse! methods
+
+Test Cases:
+  reverse([1,2,3,4]) == [4,3,2,1]          # => true
+  reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+  reverse(['abc']) == ['abc']              # => true
+  reverse([]) == []                        # => true
+
+  list = [1, 3, 2]                      # => [1, 3, 2]
+  new_list = reverse(list)              # => [2, 3, 1]
+  list.object_id != new_list.object_id  # => true
+  list == [1, 3, 2]                     # => true
+  new_list == [2, 3, 1]                 # => true
+
+Data Structures:
+  -Arrays
+
+Algorithm:
+  -Takes in an array as a parameter
+  -initialize an empty array to 'new_arr'
+  -iterate through input array
+    -starting at the end of the original array push the element to the 'new_arr'
+  -return 'new_arr'
+
+=end
+
+def reverse(arr)
+  reversed_arr = []
+  (1..(arr.length)).each do |index|
+    reversed_arr.push(arr[-index])
+  end
+  reversed_arr
+end
+
+# p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+# p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+# p reverse(['abc']) == ['abc']              # => true
+# p reverse([]) == []                        # => true
+
+# list = [1, 3, 2]                      # => [1, 3, 2]
+# new_list = reverse(list)              # => [2, 3, 1]
+# p list.object_id != new_list.object_id  # => true
+# p list == [1, 3, 2]                     # => true
+# p new_list == [2, 3, 1]                 # => true
+
+
+###############
+
+=begin
+
+Write a method that takes two Arrays as arguments, and returns an Array that 
+contains all of the values from the argument Arrays. There should be no duplication 
+of values in the returned Array, even if there are duplicates in the original Arrays.
+
+Understanding the Problem:
+  -Input: 2 arrays
+  -Output: 1 array that contains all elements of the 2 input arrays
+
+  -Rules:
+    -2 input arrays
+    -Create a new array and populate it with the elements of the 2 input arrays
+    -There should be no duplicate elements in the new array
+    -return new array
+
+Test Cases:
+  merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+Data Structures:
+  -Arrays
+
+Algorithm:
+  -set 'combined_arr' to the input arrays concatenated together
+  -find all unique values in 'combine_arr'
+  -return 'combined_arr'
+  
+=end
+
+def merge(arr1, arr2)
+  (arr1 + arr2).uniq
+
+  # OR
+
+  arr1 | arr2
+end
+
+# p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+###############
+
+=begin
+
+Write a method that takes an Array as an argument, and returns two 
+Arrays (as a pair of nested Arrays) that contain the first half and 
+second half of the original Array, respectively. If the original array 
+contains an odd number of elements, the middle element should be placed 
+in the first half Array.
+
+Understannding the Problem:
+  -Input: Array
+  -Output: Array with 2 Arrays nested inside
+
+  -Rules:
+    -Method takes an array as an argument
+    -Will return a single array with 2 arrays nested within
+      -The first nested array contains the first half of elements from the input array
+      -The second nested array contains the second half of elements from the input array
+      -If input array contains an odd number of elements 
+        -The middle element should be placed in the first nested array
+Test Cases:
+  halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+  halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+  halvsies([5]) == [[5], []]
+  halvsies([]) == [[], []]
+
+Data Structures:
+  -Arrays
+
+Algorithm:
+  -Set 'first_arr' and 'second_arr' both to []
+  -Divide length of input array in half and set divided length to 'half_length'
+  -Iterate through input array
+    -If length of input arr is odd
+      -Push elements from input array to first array a number of times equal to 'half length' plus one
+    -Otherwise 
+      -Push elements from input array to first array a number of times equal to 'half length'
+    -Push the remainding elements from input arr to second_arr
+  -Push first array and second array to a parent and return
+
+=end
+
+def halvsies(input_arr)
+  if input_arr.length.odd?
+    idx = (input_arr.length / 2)
+  else
+    idx = (input_arr.length / 2) - 1
+  end
+  input_arr.partition { |element| input_arr.index(element) <= idx }
+end
+
+# p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+# p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+# p halvsies([5]) == [[5], []]
+# p halvsies([]) == [[], []]
+
+###############
+
+=begin
+
+Given an unordered array and the information that exactly one value in 
+the array occurs twice (every other value occurs exactly once), how would 
+you determine which value occurs twice? Write a method that will find and 
+return the duplicate value that is known to be in the array.
+
+Understannding the Problem:
+  -Input: Array
+  -Output: integer, or other object depending on the warray elements
+  
+  -Rules:
+    -Method takes in an array as an argument
+    -The inout array consists of an assortment of elements
+    -The array elements are all unique with the exception of one element.
+    -Find the element that has a duplicate and return it
+
+Test Cases:
+  find_dup([1, 5, 3, 1]) == 1
+  find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+            38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+            14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+            78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+            89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+            41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+            55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+            85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+            40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+            7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
+
+Data Structures:
+  -Arrays, hash
+
+Algorithm:
+  -Tally the input array
+  -Find the element thats value is greater than 1
+  -Return the key
+
+=end
+
+def find_dup(arr)
+  (arr.tally.keep_if { |key, value| value > 1 }).keys[0]
+end
+
+# p find_dup([1, 5, 3, 1]) == 1
+# p find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+#           38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+#           14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+          # 78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+          # 89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+          # 41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+          # 55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+          # 85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+          # 40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          # 7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
+
+###############
+
+=begin
+
+Write a method named include? that takes an Array and a search value as 
+arguments. This method should return true if the search value is in the array, 
+false if it is not. You may not use the Array#include? method in your solution.
+
+Understannding the Problem:
+  -Input: Array and a search value
+  -Output: Boolean
+
+  -Rules:
+    -Method takes in an array and a value to be searched for within the array
+    -If the method finds the search value in the input array
+      -return true
+    -Otherwise
+      -return false
+
+  -Questions:
+    -What is a search value?
+      -It appears any valid values that an array can contain
+
+Test Cases:
+  include?([1,2,3,4,5], 3) == true
+  include?([1,2,3,4,5], 6) == false
+  include?([], 3) == false
+  include?([nil], nil) == true
+  include?([], nil) == false
+
+Data Structures:
+  -Array
+
+Algorithm:
+  -Iterate through the input array
+    -compare equality between each element and the search value
+    -If the search value is found in the array
+      -Return true
+    -Otherwise
+      -Return false
+
+=end
+
+def include?(arr, search_val)
+  arr.each do |element|
+    return true if element == search_val
+  end
+  false
+end
+
+# p include?([1,2,3,4,5], 3) == true
+# p include?([1,2,3,4,5], 6) == false
+# p include?([], 3) == false
+# p include?([nil], nil) == true
+# p include?([], nil) == false
+
+###############
+
+=begin
+
+Write a method that takes a positive integer, n, as an argument, and displays 
+a right triangle whose sides each have n stars. The hypotenuse of the triangle 
+(the diagonal side in the images below) should have one end at the lower-left 
+of the triangle, and the other end at the upper-right.
+
+Understannding the Problem:
+  -Input: Positive integer
+  -Output: String, in specific formation
+
+  -Rules:
+    -Thge integer input will dictate the height and width of a triangle
+    -The triangle will be displayed using the star key (*)
+    -The triangle height and width will be equal to the input integer
+    -The triangle will start at the bottom right
+    -The acute points of the triangle will be at the bottom left and top right of the triangle
+
+Test Cases:
+triangle(5)
+
+    *
+   **
+  ***
+ ****
+*****
+
+triangle(9)
+
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
+
+Data Structures:
+  -Array and strings
+
+Algorithm:
+  -Call the #times method on the input number
+    -Puts the result of
+      -multiply the input number with the string '*' and 
+      -right justify with an argument of input number
+
+=end
+
+def triangle(num)
+  num.downto(1).each do |n|
+    puts ('*' * (n)).ljust(num)
+  end
+end
+
+# triangle(3)
+# triangle(5)
+# triangle(10)
+# triangle(15)
